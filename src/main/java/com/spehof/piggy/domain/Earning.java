a78326e85713fd23e783a2f77aedacaeffa26409
Roman Volkov <volkovsws@gmail.com>
2021-04-09 18:25:14 +0300

    refactoring Earning entity class

--- src/main/java/com/spehof/piggy/domain/Earning.java
+++ src/main/java/com/spehof/piggy/domain/Earning.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
+import java.util.Collections;
 
 /**
  * @author Spehof
@@ -14,7 +15,7 @@ import javax.persistence.*;
 @Table(name = "earnings")
 @Data
 @EqualsAndHashCode(of = {"id", "amount"})
-public class Earning {
+public class Earning extends BaseEntity{
 
     @Id
     @Column(name = "account_id")
@@ -26,4 +27,19 @@ public class Earning {
     Account account;
 
     Long amount;
+
+    public void setAccount(Account account) {
+        //prevent endless loop
+        if (sameAsFormer(this.account, account))
+            return ;
+        //set new owner
+        Account oldAccount = this.account;
+        this.account = account;
+        //remove from the old owner
+        if (oldAccount!=null)
+            oldAccount.removeEarning(this);
+        //set myself into new owner
+        if (account!=null)
+            account.setEarning(Collections.singletonList(this));
+    }
 }
